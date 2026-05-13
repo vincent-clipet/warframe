14ace8da3ff5a880cd96ab05da7d333f6bfc4ada
Vincent Clipet <vincent.clipet.7@gmail.com>
2026-05-13 15:18:30 +0200

    fix: Rank 3 arcanes were not processed for pricing history

--- get_arcane_prices.js
+++ get_arcane_prices.js
@@ -63,33 +63,33 @@ for (const arcane of arcanes) {
     const statistics_full = data.payload.statistics_closed["90days"]
     const statistics = statistics_full.filter(e => moment(e.datetime).isSameOrAfter(lastWeek, "day"))
     const pricing_data = {
-        moving_avg_rank0: 0,
-        volume_rank0: 0,
-        moving_avg_rank5: 0,
-        volume_rank5: 0,
+        moving_avg_rank_0: 0,
+        volume_rank_0: 0,
+        moving_avg_rank_max: 0,
+        volume_rank_max: 0,
     }
 
     for (const stat of statistics) {
-        if (stat.mod_rank === 5) {
-            pricing_data.volume_rank5 += stat.volume ? stat.volume : 0
-            pricing_data.moving_avg_rank5 = (pricing_data.moving_avg_rank5 === 0 && stat.moving_avg) ? stat.moving_avg : pricing_data.moving_avg_rank5
+        if (stat.mod_rank === 5 || stat.mod_rank === 3) {
+            pricing_data.volume_rank_max += stat.volume ? stat.volume : 0
+            pricing_data.moving_avg_rank_max = (pricing_data.moving_avg_rank_max === 0 && stat.moving_avg) ? stat.moving_avg : pricing_data.moving_avg_rank_max
         } else if (stat.mod_rank === 0) {
-            pricing_data.volume_rank0 += stat.volume ? stat.volume : 0
-            pricing_data.moving_avg_rank0 = (pricing_data.moving_avg_rank0 === 0 && stat.moving_avg) ? stat.moving_avg : pricing_data.moving_avg_rank0
+            pricing_data.volume_rank_0 += stat.volume ? stat.volume : 0
+            pricing_data.moving_avg_rank_0 = (pricing_data.moving_avg_rank_0 === 0 && stat.moving_avg) ? stat.moving_avg : pricing_data.moving_avg_rank_0
         }
     }
 
     const str = [
         arcane.name,
-        pricing_data.moving_avg_rank0.toString().replace('.', ','),
-        pricing_data.volume_rank0,
-        pricing_data.moving_avg_rank5.toString().replace('.', ','),
-        pricing_data.volume_rank5,
+        pricing_data.moving_avg_rank_0.toString().replace('.', ','),
+        pricing_data.volume_rank_0,
+        pricing_data.moving_avg_rank_max.toString().replace('.', ','),
+        pricing_data.volume_rank_max,
     ].join("\t")
     lines.push(str)
     console.log(str)
 
-    await sleep(200); // Cloudflare is not a fan of 150+ requests in a row
+    await sleep(100); // Cloudflare is not a fan of 150+ requests in a row
 }
 
 await clipboard.write(lines.join("\n"));
